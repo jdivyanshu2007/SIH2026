2784045e58483808905ae2f7b696de362784c9d4
jdivyanshu2007 <jdivyanshu2007@gmail.com>
2026-09-03 12:35:44 +0530

    add backend folder

---
+++ backend/FoodLabel/src/main/java/com/SIH/FoodLabel/FoodLabelApplication.java
@@ -0,0 +1,13 @@
+package com.SIH.FoodLabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodLabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodLabelApplication.class, args);
+	}
+
+}
---
+++ backend/FoodLabel/src/test/java/com/SIH/FoodLabel/FoodLabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SIH.FoodLabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
